8c19528af1e8f5d13629d472d29af289a2991214
Ashish Datta <ashish@setfive.com>
2013-01-29 23:28:59 -0500

    added the the-bootstrap base theme added the styles for the homepage added the wordpress importer

--- wp-content/themes/setfive_three/js/main.js
+++ wp-content/themes/setfive_three/js/main.js
@@ -10,56 +10,64 @@ jQuery(document).ready(function($){
        $(this).animate({color: $(this).data("color")}, 150);
    });   
    
-   var sidebarZeroColor = jQuery.Color( jQuery(".content-sidebar h4:first").css("background-color") );
-   var sidebarTargetColor = jQuery.Color( "#EA4C88" );
+   function enableSidebarScrollspy(){
    
-   var sidebarPosition = $(".entry-content .content-sidebar").position();
-   var entryContentOffsets = { };   
+	   var sidebarZeroColor = jQuery.Color( jQuery(".content-sidebar h4:first").css("background-color") );
+	   var sidebarTargetColor = jQuery.Color( "#EA4C88" );
+	   
+	   var sidebarPosition = $(".entry-content .content-sidebar").position();
+	   var entryContentOffsets = { };   
+	   
+	   $(".entry-content h3").each(function(){
+	       entryContentOffsets[ $(this).text() ] = $(this).nextAll("p:first").position().top;
+	   });
+	   
+	   jQuery(".content-sidebar h4 a").click(function(){       
+	       var pos = 0;
+	       
+	       if( jQuery(this).text() in entryContentOffsets ){
+	           pos = jQuery(".entry-content h3:contains(" + jQuery(this).text() + ")").position().top;
+	       }
+	       
+	       jQuery(window).scrollTop( pos ).scroll();
+	       return false;
+	   });
+	   
+	   $(window).scroll(function(){
+	       
+	       var scrollSidebar = function(){
+	           
+	           var windowTop = jQuery(window).scrollTop();
+	           var diff;
+	           
+	           if( jQuery(".entry-content .content-sidebar").position().top - sidebarPosition.top < windowTop ){
+	               diff = jQuery(window).scrollTop() - jQuery(".entry-content .content-sidebar").position().top;               
+	           }else{
+	               diff = 0;
+	           }
+	           
+	           jQuery(".entry-content .content-sidebar").animate({"padding-top": diff}, 150);
+	           
+	           jQuery(".content-sidebar h4").each(function(){
+	               var pos = $(this).text() in entryContentOffsets ? entryContentOffsets[ $(this).text() ] : 0;
+	               var percent = (windowTop / pos) < 1 ? (windowTop / pos) : 1;               
+	               
+	               var newBg = sidebarZeroColor.transition( sidebarTargetColor, percent );               
+	               $(this).animate( {"background-color": newBg.toHexString() } );
+	           });
+	       }
+	       
+	       if( window.sidebarTimeout ){
+	           window.sidebarTimeout = window.clearTimeout( window.sidebarTimeout );
+	       }
+	       
+	       window.sidebarTimeout = window.setTimeout( scrollSidebar, 200 );       
+	   });
+	   
+   }
    
-   $(".entry-content h3").each(function(){
-       entryContentOffsets[ $(this).text() ] = $(this).nextAll("p:first").position().top;
-   });
-   
-   jQuery(".content-sidebar h4 a").click(function(){       
-       var pos = 0;
-       
-       if( jQuery(this).text() in entryContentOffsets ){
-           pos = jQuery(".entry-content h3:contains(" + jQuery(this).text() + ")").position().top;
-       }
-       
-       jQuery(window).scrollTop( pos ).scroll();
-       return false;
-   });
-   
-   $(window).scroll(function(){
-       
-       var scrollSidebar = function(){
-           
-           var windowTop = jQuery(window).scrollTop();
-           var diff;
-           
-           if( jQuery(".entry-content .content-sidebar").position().top - sidebarPosition.top < windowTop ){
-               diff = jQuery(window).scrollTop() - jQuery(".entry-content .content-sidebar").position().top;               
-           }else{
-               diff = 0;
-           }
-           
-           jQuery(".entry-content .content-sidebar").animate({"padding-top": diff}, 150);
-           
-           jQuery(".content-sidebar h4").each(function(){
-               var pos = $(this).text() in entryContentOffsets ? entryContentOffsets[ $(this).text() ] : 0;
-               var percent = (windowTop / pos) < 1 ? (windowTop / pos) : 1;               
-               
-               var newBg = sidebarZeroColor.transition( sidebarTargetColor, percent );               
-               $(this).animate( {"background-color": newBg.toHexString() } );
-           });
-       }
-       
-       if( window.sidebarTimeout ){
-           window.sidebarTimeout = window.clearTimeout( window.sidebarTimeout );
-       }
-       
-       window.sidebarTimeout = window.setTimeout( scrollSidebar, 200 );       
-   });
+   if( jQuery(".entry-content .content-sidebar").length ){
+	   enableSidebarScrollspy();
+   }
    
 });
