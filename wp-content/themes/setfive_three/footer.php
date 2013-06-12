190be0609d6a6332b82dbcb6a84f677566bf2ce0
Shout @ setfive.com <shout@setfive.com>
2013-06-11 23:21:55 -0400

    added the quantcast tag to shout

--- wp-content/themes/setfive_three/footer.php
+++ wp-content/themes/setfive_three/footer.php
@@ -37,8 +37,35 @@
         var _gaq=[["_setAccount","<?php echo GA_ACCOUNT; ?>"],["_trackPageview"]]; 
         (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
         g.src=("https:"==location.protocol?"//ssl":"//www")+".google-analytics.com/ga.js";
-        s.parentNode.insertBefore(g,s)}(document,"script"));    
+        s.parentNode.insertBefore(g,s)}(document,"script"));        
     </script>
+	
+	<?php if( GA_ACCOUNT == "UA-3761258-7" ): ?>
+		<!-- Quantcast Tag -->
+		<script type="text/javascript">
+		var _qevents = _qevents || [];
+		
+		(function() {
+		var elem = document.createElement('script');
+		elem.src = (document.location.protocol == "https:" ? "https://secure" : "http://edge") + ".quantserve.com/quant.js";
+		elem.async = true;
+		elem.type = "text/javascript";
+		var scpt = document.getElementsByTagName('script')[0];
+		scpt.parentNode.insertBefore(elem, scpt);
+		})();
+		
+		_qevents.push({
+		qacct:"p-PL8cDEemdgPFm"
+		});
+		</script>
+		
+		<noscript>
+		<div style="display:none;">
+		<img src="//pixel.quantserve.com/pixel/p-PL8cDEemdgPFm.gif" border="0" height="1" width="1" alt="Quantcast"/>
+		</div>
+		</noscript>
+		<!-- End Quantcast tag -->
+	<?php endif; ?>
 	
 	</body>
 </html>
